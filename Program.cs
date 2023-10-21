cff9580234244d52cd071ddb4cf55cfae4a9557c
Temurbek <magicalland6@gmail.com>
2023-10-21 14:10:02 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"hello, {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter your age: ");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine("Converting . . .");
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Succesfully converted! {age}");
+int TaosAge = 10;
+int ageDeffrence = age - TaosAge;
+System.Console.WriteLine($"The deffernece between your and kescha's age is {ageDeffrence} ");
+
